d6f775217132972cbd80283e2bb4fd8dcf40a214
Tim D. Smith <git@tim-smith.us>
2016-05-13 13:50:39 -0700

    First commit

---
+++ R/logicle_scale.R
@@ -0,0 +1,18 @@
+#' Logicle transformation for ggplot2.
+#' See ?flowCore::logicletGml2 for the definition of the parameters.
+#' @export
+logicle_trans = function(w=0.5, t=262144, m=4.5) {
+  logicleTransform = flowCore::logicleTransform(w=w, t=t, m=m, a=0)
+  transform = function(x) {
+    logicleTransform(x)
+  }
+  inverse = function(x) {
+    flowCore::inverseLogicleTransform("inverseLogicle", logicleTransform)(x)
+  }
+  breaks = function(x) {
+    lim = 10**(1+w)
+    unique(c(scales::pretty_breaks(n=3, min.n=3)(c(x[1], lim)),
+             scales::log_breaks(10)(c(lim, x[2]))))
+  }
+  scales::trans_new("logicle", transform, inverse, breaks)
+}
